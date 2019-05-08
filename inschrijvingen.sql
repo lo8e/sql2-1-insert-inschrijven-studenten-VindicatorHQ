0f9d214cf22cb5bca651f9cbc697efd0e1213eed
VindicatorHQ <36315904+VindicatorHQ@users.noreply.github.com>
2019-05-08 09:49:27 +0200

    Studenten aan klassen toegevoegd

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,7 @@
-USE `lo8e_sql2`;
-
-INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) VALUES
-	(90423,'Matoi','','Ryuko','1993-07-07','2211 SG','Amsterdam', 69.9, 1.80, 01-01-2019),
-	(90874,'Gasai','','Yuno','1992-04-05','2323 EX','Leiderdorp', 79.8, 1.93, 01-01-2019),
-	(99690,'Geulkes','','Gerardsus','1993-09-26','2492 AG','Den Haag', 67.7, 1.71, 01-01-2019),
-	(95318,'Modder','','Timmmy','1993-06-11','2273 HP','Voorburg', 72.5, 1.85, 01-01-2019);     
+USE `lo8e_sql2`;  
+    
+INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
+	(90423,'lo9e-amo1'),
+	(90874,'lo9e-amo3'),
+	(99690,'lo9e-amo1'),
+	(95318,'lo9e-amo2');
