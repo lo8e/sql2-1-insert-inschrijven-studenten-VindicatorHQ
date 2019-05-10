416b5a890e6af7750538c27c0e2e0bfde17f39b5
VindicatorHQ <36315904+VindicatorHQ@users.noreply.github.com>
2019-05-10 11:52:56 +0200

    Geboortedatum veranderd

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,5 @@
 USE `lo8e_sql2`;  
     
-INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
-	(90423,'lo9e-amo1'),
-	(90874,'lo9e-amo3'),
-	(99690,'lo9e-amo1'),
-	(95318,'lo9e-amo2');
+UPDATE studenten
+SET geboortedatum = '1995-25-09'
+WHERE geboortedatum = '1993-07-27';
